f3a60a5c2867dec9daa2468b4c85d233e6b187c2
MullerDerRaumdeuter <a01745219@itesm.mx>
2022-10-06 16:57:47 -0500

    ya le entendi owo 2

--- nodes_generator.py
+++ nodes_generator.py
@@ -10,7 +10,43 @@ class NodeGenerator:
         self.width = width
         self.height = height
         #self.nodesNumber = nodesNumber
-        coordinates = [[2, height], [], ] #Aqui se anotan las coordenadas X y Y de las capitales 
+        #Aqui se anotan las coordenadas X y Y de las capitales 
+        '''
+        Las capitales iran en el siguiente orden:
+        1. Mexicalli
+        2. La Paz
+        3. Hermosillo 
+        4. Chihuahua
+        5. Saltillo
+        6. Monterrey
+        7. Ciudad Victoria
+        8. San Luis Potosi
+        9. Zacatecas
+        10. Durango
+        11. Culiacan
+        12. Tepic
+        13. Guadalajara
+        14. Colima
+        15. Aguascalientes 
+        17. Guanajuato
+        18. Queretaro
+        19. Pachuca
+        20. Xalapa
+        21. Puebla 
+        22. Cuernavaca
+        23. CDMX
+        24. Toluca
+        25. Morelia
+        26. Chilpancingo
+        27. Oaxaca
+        28. Tuxtla Gutierrez
+        29. Villahermosa
+        30. Campeche
+        31. Merida
+        32. Chetumal
+        '''
+        coordinates = [[2, height], [], ] 
+
 
     def generate(self):
         #Aqui se van a acomodar en dos arreglos, uno para X y el otro para Y, finalmente se regresa un column stack.
