a3cac6502e65b894acca821237cb6a3fc8a28c79
Eduardo Roberto Muller Romero <a01745219@itesm.mx>
2022-10-19 19:57:12 -0500

    me voy a aventar una ñerada de genetic algorithm

--- tcp_genetic.py
+++ tcp_genetic.py
@@ -36,7 +36,15 @@ Las capitales iran en el siguiente orden:
 31. Merida ()
 32. Chetumal (350, 40)
 '''
-cityIndex = {"La Paz": 0, "Mexicalli": 1, "Hermosillo":2, }, 
+cityIndex = {"La Paz": 0, "Mexicalli": 1, "Hermosillo":2, "Chihuahua": 3, "Saltillo": 4, "Monterrey": 5, "Ciudad Victoria": 6, "San Luis Potosi": 7, "Zacatecas": 8,
+             "Durango": 9, "Culiacan": 10, "Tepic":11, "Guadalajara":12, "Colima": 13, "Aguascalientes": 14, "Tlaxcala":15, "Guanajuato": 16, "Queretaro": 17, "Pachuca":18,
+             "Xalapa": 19, "Puebla":20, "Cernavaca":21, "CDMX":22, "Toluca":23, "Morelia":24, "Chilpancingo":25, "Oaxaca":26, "Tuxtla Gutierrez": 27, "Villahermosa": 28,
+             "Campeche":29, "Merida":30, "Chetumal": 31}
+
+cities = [[40, 280], [90, 130], [80, 200], [140, 180], [200, 140], [280, 150], [290, 120], [200, 100], [190, 180], [160, 130], [130, 140], [150, 90], 
+               [190, 80], [180, 70], [180, 170], [230, 70], [190, 90], [200, 80], [220, 70], [240, 70], [230, 60], [210, 70], [200, 80], [190, 70], [220, 30], 
+               [250, 30], [290, 20], [280, 30], [320, 60], [330, 80], [350, 40]] 
+ 
 coordinates = [[40, 280], [90, 130], [80, 200], [140, 180], [200, 140], [280, 150], [290, 120], [200, 100], [190, 180], [160, 130], [130, 140], [150, 90], 
                [190, 80], [180, 70], [180, 170], [230, 70], [190, 90], [200, 80], [220, 70], [240, 70], [230, 60], [210, 70], [200, 80], [190, 70], [220, 30], 
                [250, 30], [290, 20], [280, 30], [320, 60], [330, 80], [350, 40]] 
@@ -47,12 +55,29 @@ def calculateDist(a, b):
                      + ((a[1] - b[1]) **2))
                     ) 
 
-def main():
+def geneticAlgorithm(coordinates, cityIndexa, cities):
+    ruta = ""
     la_paz = coordinates[0]
     dist_lapaz_mexicalli = calculateDist(la_paz, coordinates[1])
     dist_mexicalli_hermosillo = calculateDist(coordinates[1], coordinates[2])
     tramoforzosoA = dist_lapaz_mexicalli + dist_mexicalli_hermosillo
-    print("Distancia del tramo forzoso: ", tramoforzosoA)
+    coordinates.pop(0)
+    coordinates.pop(0)
+    counter = 2 #Porque ya eliminamos dos, esto sirve para identificar la ruta
+    ruta = "La Paz --> Mexicalli --> Hermosillo"
+    while coordinates:
+        ruta += "-->"
+        start = coordinates[0]
+        if len(coordinates) > 1:
+            dist_list = []
+            for i in range(1, len(coordinates)-1):
+                dist_list.append(calculateDist(start, coordinates[i]))
+            dist = min(dist_list)
+
+def main():
+    geneticAlgorithm(coordinates, cityIndex, cities)
+
+    
     
     
 
